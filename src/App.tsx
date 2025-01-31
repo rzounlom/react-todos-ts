7a69b81e9049c901214f37181801718269cd34ee
rzounlom <rzounlom@gmail.com>
2025-01-30 21:04:25 -0500

    moved default todos to state

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import "./App.css";
 import { Button } from "react-bootstrap";
 import TodoList from "./components/TodoList";
 import { defaultTodos } from "./data";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodoes] = useState(defaultTodos);
+
   return (
     <div className="container">
       <h1>Todo App Example</h1>
       <Button className="mt-2 mb-2">Add Todo</Button>
       <TodoList
-        todos={defaultTodos}
+        todos={todos}
         // toggleComplete={toggleComplete}
         // deleteTodo={deleteTodo}
       />
